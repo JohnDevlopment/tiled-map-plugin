722e4422a1a09110e664518a1b94020eefceb59c
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-11 01:00:31 -0600

    Update tileset

--- assets/tilesets/pixel_platformer.tsx
+++ assets/tilesets/pixel_platformer.tsx
@@ -1,28 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="pixel_platformer" tilewidth="18" tileheight="18" tilecount="260" columns="20">
  <image source="../textures/tilesets/pixel_platformer.png" width="360" height="234"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="18" height="18"/>
-  </objectgroup>
- </tile>
- <tile id="153">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="one_way" x="0" y="0" width="18" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="154">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="one_way" x="0" y="0" width="18" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="155">
-  <objectgroup draworder="index" id="2">
-   <object id="1" type="one_way" x="0" y="0" width="18" height="8"/>
-  </objectgroup>
- </tile>
  <wangsets>
-  <wangset name="ground" type="corner" tile="22">
+  <wangset name="grass ledge platforms" type="edge" tile="-1">
+   <wangcolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="184" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="185" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="186" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="187" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="204" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="205" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="206" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="207" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="224" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="225" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="226" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="227" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="245" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="246" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="247" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+  <wangset name="grass ledges" type="corner" tile="22">
    <wangcolor name="grass" color="#ff0000" tile="22" probability="1"/>
    <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
    <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
@@ -38,15 +36,21 @@
    <wangtile tileid="142" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="143" wangid="0,0,0,0,0,0,0,1"/>
   </wangset>
-  <wangset name="shroom platform" type="edge" tile="13">
-   <wangcolor name="shroom" color="#ff0000" tile="-1" probability="1"/>
-   <wangtile tileid="12" wangid="0,0,1,0,1,0,1,0"/>
-   <wangtile tileid="13" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="14" wangid="0,0,1,0,0,0,0,0"/>
-   <wangtile tileid="15" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="32" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="52" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="72" wangid="1,0,0,0,0,0,0,0"/>
+  <wangset name="dead grass ledges" type="corner" tile="62">
+   <wangcolor name="dead grass" color="#ff0000" tile="42" probability="1"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="24" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="25" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="61" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="62" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="63" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="121" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="122" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="123" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="141" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="142" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="143" wangid="0,0,0,0,0,0,0,1"/>
   </wangset>
   <wangset name="pipes" type="edge" tile="180">
    <wangcolor name="pipes" color="#ff0000" tile="-1" probability="1"/>
