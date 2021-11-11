ac05e181e770d5d5e60357846bd971d29b034acd
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-11 00:21:47 -0600

    Deleted unnecessary files

--- assets/tilesets/pixel_platformer_bg.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="pixel_platformer_bg" tilewidth="18" tileheight="18" tilecount="16" columns="8">
- <image source="../textures/tilesets/pixel_platformer_bg.png" width="144" height="36"/>
-</tileset>
